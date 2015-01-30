7868fc5fb1caec198de4b8085a801ed09bea0ad7
Anna Akullian <annaakullian@Annas-MacBook-Air-2.local>
2015-01-30 13:11:42 -0800

    preliminary structure

--- main.py
+++ main.py
@@ -7,5 +7,13 @@ app = Flask(__name__)
 def login_page():
 	return render_template("login.html")
 
+@app.route("/scholarmain")
+def scholarmain():
+	return render_template("scholarmain.html")
+
+@app.route("/staffmain")
+def staffmain():
+	return render_template("staffmain.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
